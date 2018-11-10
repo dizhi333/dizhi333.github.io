54daf53c9b14d7938c6e541b67b00a8b12e85c2d
dizhi333 <44903674+dizhi333@users.noreply.github.com>
2018-11-11 06:01:19 +0800

    Update yuan.sh

--- yuan.sh
+++ yuan.sh
@@ -16,6 +16,7 @@ getAbout() {
 	echo " \            Choose a faster mirror for Linux           / "
 	echo " \       Version: ${GREEN}1.0.3${PLAIN} (2 Nov 2018)     / "
 	echo " ========================================================= "
+	echo -e " Version: ${GREEN}1.0.3${PLAIN} (2 Nov 2018) "
 	echo " wget -O yuan.sh https://git.io/fpt02 && sudo bash yuan.sh"
 	echo ""
 }
